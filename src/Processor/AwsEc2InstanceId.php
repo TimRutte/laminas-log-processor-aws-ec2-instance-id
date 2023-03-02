61b14211d5cf3c55870542436c87479b49d402c3
Tim Rutte <info@timrutte.de>
2023-03-02 20:29:02 +0100

    added instance id fetching

--- src/Processor/AwsEc2InstanceId.php
+++ src/Processor/AwsEc2InstanceId.php
@@ -9,7 +9,10 @@ class AwsEc2InstanceId implements ProcessorInterface
     public function process(array $event) :array
     {
         if (!isset($event['instance_id'])) {
-            $event['instance_id'] = '';
+            $instanceId = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
+            if ($instanceId !== false) {
+                $event['instance_id'] = $instanceId;
+            }
         }
 
         return $event;
